860a79065c35b3f59e54400ed9e027d94a3a35be
Pedro Nalvarte <impedro2406@gmail.com>
2024-08-12 01:15:52 -0500

    v25

--- back/lambdaprueba/index.js
+++ back/lambdaprueba/index.js
@@ -1,8 +1,17 @@
 export const handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Esto es una prueba de despliegue de mi lambda'),
-    };
-    return response;
+    try {
+        // Lógica principal de tu Lambda
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify('Esto es una prueba de despliegue de mi lambda'),
+        };
+        return response;
+    } catch (error) {
+        // Manejo de errores
+        console.error('Error en la ejecución de la Lambda:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify('Ocurrió un error en la Lambda'),
+        };
+    }
 };
